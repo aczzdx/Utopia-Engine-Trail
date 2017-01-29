fd9b15d96937d65f83365d4be37a57cc1a4612d2
xingyuwei <aczzdx@gmail.com>
2017-01-29 01:54:13 -0600

    Create Event.py

---
+++ Event.py
@@ -0,0 +1,10 @@
+#####################################
+# This file defines Event,
+# an interface for every event happened
+# in the game.
+###############################
+
+class Event():
+
+    def __init__(self):
+        self.desc = "The abstract event"
